8c4386424529fa1f537b0f37b94201b4c5ba1705
bagool <ioan.adrian98@gmail.com>
2018-02-07 17:44:54 +0000

    declared Token class

--- TrashPanda/Token.h
+++ TrashPanda/Token.h
@@ -5,11 +5,123 @@
 #ifndef RACCOONLANG_TOKEN_H
 #define RACCOONLANG_TOKEN_H
 
+#include <string>
+#include <regex>
+#include <map>
+
 namespace RCC {
 
-class Token {
+    enum class OperatorTypes {
+        // unary
+        OT_INCREMENT, // ++
+        OT_DECREMENT, // --
+        OT_LSHIFT,    // <<
+        OT_RSHIFT,    // >>
+        // binary
+        OT_PLUS,      // +
+        OT_MINUS,     // -
+        OT_DIV,       // / (division)
+        OT_MULT,      // * (multiplication)
+        OT_MOD,       // % (modulo)
+        OT_ASSIGN,    // =
+        // logical
+        OT_GT,        // > (greater than)
+        OT_GTET,      // >= (greater than or equal to)
+        OT_LT,        // < (lesser than)
+        OT_LTET,      // <= (lesser than or equal to)
+        // braces
+        OT_CURLY_OPEN,  // {
+        OT_CURLY_CLOSE, // }
+        OT_ROUND_OPEN,  // (
+        OT_ROUND_CLOSE, // )
+        // punctuation
+        OT_EOS,         // ; (end of statement)
+        OT_COMMA        // ,
+    };
 
-};
+    enum class KeywordTypes {
+        KT_DECL,	// let - declaration
+        KT_FUNC,	// function
+        KT_IF,		// if statement
+        KT_WHILE,	// while statement
+        KT_TRUE,	// true (1)
+        KT_FALSE    // false (0)
+    };
+
+    enum class LiteralTypes {
+        LT_ID,  // user defined identifier
+        LT_STR, // string literal
+        LT_INT, // integer literal
+        LT_DEC  // decimal literal
+    };
+    /* comparator for std::map */
+    struct comparator {
+        bool operator() (const std::string& x, const std::string& y) {
+            return x < y;
+        }
+    };
+
+    const std::map<const std::string, const OperatorTypes, comparator> mapped_operators {
+        // unary
+        { "++", OperatorTypes::OT_INCREMENT },    // ++
+        { "--", OperatorTypes::OT_DECREMENT },    // --
+        { "<<", OperatorTypes::OT_LSHIFT    },    // <<
+        { ">>", OperatorTypes::OT_RSHIFT    },    // >>
+        // binary
+        { "+", OperatorTypes::OT_PLUS   },     // +
+        { "-", OperatorTypes::OT_MINUS  },     // -
+        { "/", OperatorTypes::OT_DIV    },     // / (division)
+        { "*", OperatorTypes::OT_MULT   },     // * (multiplication)
+        { "%", OperatorTypes::OT_MOD    },     // % (modulo)
+        { "=", OperatorTypes::OT_ASSIGN },	   // =
+        // logical
+        { ">" , OperatorTypes::OT_GT   },    // > (greater than)
+        { ">=", OperatorTypes::OT_GTET },	 // >= (greater than or equal to)
+        { "<",  OperatorTypes::OT_LT   },    // < (lesser than)
+        { "<=", OperatorTypes::OT_LTET },	 // <= (lesser than or equal to)
+        // braces
+        { "{", OperatorTypes::OT_CURLY_OPEN  },   // {
+        { "}", OperatorTypes::OT_CURLY_CLOSE },   // }
+        { "(", OperatorTypes::OT_ROUND_OPEN  },   // (
+        { ")", OperatorTypes::OT_ROUND_CLOSE },   // )
+        // punctuation
+        { ";",  OperatorTypes::OT_EOS   },	    // ; (end of statement)
+        { ",",  OperatorTypes::OT_COMMA },	    // ,
+    };
+
+    const std::map<const std::string, const KeywordTypes, comparator> mapped_keywords {
+        { "let",      KeywordTypes::KT_DECL  },	 // let - declaration
+        { "function", KeywordTypes::KT_FUNC  },	 // function
+        { "if",       KeywordTypes::KT_IF	 },  // if statement
+        { "while",    KeywordTypes::KT_WHILE },	 // while statement
+        { "true",     KeywordTypes::KT_TRUE  },  // true
+        { "false",    KeywordTypes::KT_FALSE }   // false
+    };
+
+    const std::regex decimal_literals("[0-9]*\\.?[0-9]+");
+    const std::regex integer_literals("d+");
+    const std::regex string_literal("\"([^\\\"]|\\.)*\"");
+    const std::regex identifier_literal("(([A-Z]|[a-z]|_)([A-Z]|[0-9]|[a-z]|_)?)+");
+
+    union TokenType {
+        OperatorTypes operator_;
+        KeywordTypes keyword_;
+        LiteralTypes literal_;
+    };
+
+    class Token {
+        // prevent the creation of default constructor
+        Token() {}
+        // the token type and name will be set by the constructor
+        std::string _token_name = "";
+        TokenType _token_type;
+    public:
+        Token(const std::string& token_type);
+        const std::string get_token_name() { return _token_name; }
+        const TokenType get_token_type() { return _token_type; }
+        inline bool is_set();
+        void print();
+    };
 
 }
 
