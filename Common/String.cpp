be408563a6c15fdfbaab887d0b99908470883d39
bagool <ioan.adrian98@gmail.com>
2018-02-07 19:01:13 +0000

    finished custom string class

--- Common/String.cpp
+++ Common/String.cpp
@@ -2,23 +2,41 @@
 // Created by bagool on 07/02/2018.
 //
 #include <algorithm>
+#include <iostream>
 
 #include "String.h"
 
+using namespace RCC;
+
 /* trim the left-hand side of a string */
-static inline void String::_ltrim(std::string &s) {
+void String::_ltrim(String &s) {
     s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) {
         return !std::isspace(ch);
     }));
 }
 /* trim the right-hand side of a string */
-static inline void String::_rtrim(std::string &s) {
+void String::_rtrim(String &s) {
     s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
         return !std::isspace(ch);
     }).base(), s.end());
 }
 /* trim a string */
-static inline void String::trim(std::string &s) {
-    ltrim(s);
-    rtrim(s);
+void String::trim() {
+    _ltrim(*this);
+    _rtrim(*this);
+}
+
+std::vector<String> String::split(const char delimiter) {
+    String temp = *this;
+    String token;
+    std::vector<String> return_vect;
+    size_t pos = 0;
+
+    while ((pos = temp.find(delimiter)) != String::npos) {
+        token = temp.substr(0, pos);
+        return_vect.push_back(token);
+        temp.erase(0, pos + 1);
+    }
+
+    return return_vect;
 }
--- Common/String.h
+++ Common/String.h
@@ -5,15 +5,27 @@
 #ifndef RACCOONLANG_STRING_H
 #define RACCOONLANG_STRING_H
 #include <string>
+#include <vector>
 
-class String {
-    static inline void _ltrim(std::string&);
-    static inline void _rtrim(std::string&);
-    // prevent the creation of default constructor
-    String() {}
-public:
-    static void trim(std::string &s);
-};
+namespace RCC {
+    /* string with trim and split functionality */
+    class String : public std::basic_string<char> {
+        typedef std::basic_string<char> str;
+        void _ltrim(String&);
+        void _rtrim(String&);
+    public:
+        String() : str() {}
+        explicit String(const char *s) : str(s) {}
+        String(const char *s, size_t n) : str(s, n) {}
+        String(size_t n, char c) : str(n, c) {}
 
+        void trim();
+        std::vector<String> split(char);
+
+        explicit operator const char*() {
+            return c_str();
+        }
+    };
+}
 
 #endif //RACCOONLANG_STRING_H
